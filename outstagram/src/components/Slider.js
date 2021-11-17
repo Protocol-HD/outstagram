4f9177fc35eac8538ec1fdd10667d300eac1b4ef
YJ Ahn <beat1103@gmail.com>
2021-11-17 16:16:20 +0900

    update

--- outstagram/src/components/Slider.js
+++ outstagram/src/components/Slider.js
@@ -1,86 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SliderItem from './widgets/slider/SliderItem';
 
 function Slider() {
+
+	const [sliders, setSliders] = useState([])
+
+	useEffect(()=>{
+		fetch("http://localhost:3010/Sliders")
+		.then(res=>{
+			return res.json()
+		})
+		.then(data=>{
+			setSliders(data)
+			console.log(data)
+		})
+
+		
+	},[])
 	return (
 		<div className="eskimo-carousel-container eskimo-bg-loader">
 			<div id="eskimo-post-slider" className="eskimo-slider">
-				<div>
-					<a className="eskimo-slider-img" href="single-post.html"></a>
-					<ul className="eskimo-slider-image-meta eskimo-image-meta-post">
-						<li><a href="single-post.html"><span className="badge badge-default">May 28, 2018</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Food &amp; Drink</span></a></li>
-					</ul>
-					<div className="clearfix"></div>
-					<img src="images/1200x600.png" alt="Ketchup Flavored Ice Cream!" />
-					<div className="eskimo-slider-desc">
-						<div className="eskimo-slider-desc-inner">
-							<h2 className="card-title">Ketchup Flavored Ice Cream!</h2>
-							<p>Mandaremus illum possumus ullamco e qui de nisi enim anim. Si nulla si nisi, veniam litteris sed excepteur ne ut amet deserunt tempor nam expetendis de enim mandaremus. Ita ea minim esse cillum ut eram arbitror ullamco. Tamen ad expetendis ab ipsum proident</p>
-						</div>
-					</div>
-				</div>
-				<div>
-					<a className="eskimo-slider-img" href="single-post.html"></a>
-					<ul className="eskimo-slider-image-meta eskimo-image-meta-post">
-						<li><a href="single-post.html"><span className="badge badge-default">May 27, 2018</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Lifestyle</span></a></li>
-					</ul>
-					<div className="clearfix"></div>
-					<img src="images/1200x600.png" alt="Hair You've Always Dreamed Of" />
-					<div className="eskimo-slider-desc">
-						<div className="eskimo-slider-desc-inner">
-							<h2 className="card-title">Hair You've Always Dreamed Of</h2>
-							<p>Ex si esse deserunt, et dolore occaecat, singulis tamen e possumus voluptatibus, varias cernantur si aute quid. Noster vidisse iis exquisitaque, deserunt te irure. Ubi illum nostrud fidelissimae id occaecat duis probant. Occaecat hic quorum aliquip se</p>
-						</div>
-					</div>
-				</div>
-				<div>
-					<a className="eskimo-slider-img" href="single-post.html"></a>
-					<ul className="eskimo-slider-image-meta eskimo-image-meta-post">
-						<li><a href="single-post.html"><span className="badge badge-default">May 25, 2018</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Lifestyle</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Travel</span></a></li>
-					</ul>
-					<div className="clearfix"></div>
-					<img src="images/1200x600.png" alt="15 Of The World's Best Carnivals" />
-					<div className="eskimo-slider-desc">
-						<div className="eskimo-slider-desc-inner">
-							<h2 className="card-title">15 Of The World's Best Carnivals</h2>
-							<p>Aliquip e duis. Se labore ullamco excepteur iis ullamco sint duis laboris amet sed ita occaecat de cernantur quo fore coniunctione voluptate enim senserit. Si ut nulla laboris, an eiusmod e incididunt. Non varias enim duis singulis, a quorum cupidatat. Voluptate summis</p>
-						</div>
-					</div>
-				</div>
-				<div>
-					<a className="eskimo-slider-img" href="single-post.html"></a>
-					<ul className="eskimo-slider-image-meta eskimo-image-meta-post">
-						<li><a href="single-post.html"><span className="badge badge-default">May 25, 2018</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Lifestyle</span></a></li>
-					</ul>
-					<div className="clearfix"></div>
-					<img src="images/1200x600.png" alt="5 Ways to a Healthy Lifestyle" />
-					<div className="eskimo-slider-desc">
-						<div className="eskimo-slider-desc-inner">
-							<h2 className="card-title">5 Ways to a Healthy Lifestyle</h2>
-							<p>Mandaremus veniam ab cupidatat exquisitaque, e quae laboris domesticarum, non sint mentitum fabulas de anim proident transferrem, ita aliqua imitarentur in in labore illum eram offendit, nisi fidelissimae possumus noster ullamco se eiusmod multos ex</p>
-						</div>
-					</div>
-				</div>
-				<div>
-					<a className="eskimo-slider-img" href="single-post.html"></a>
-					<ul className="eskimo-slider-image-meta eskimo-image-meta-post">
-						<li><a href="single-post.html"><span className="badge badge-default">May 23, 2018</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Food &amp; Drink</span></a></li>
-						<li><a href="category.html"><span className="badge badge-default">Travel</span></a></li>
-					</ul>
-					<div className="clearfix"></div>
-					<img src="images/1200x600.png" alt="Best Breakfast In The World" />
-					<div className="eskimo-slider-desc">
-						<div className="eskimo-slider-desc-inner">
-							<h2 className="card-title">Best Breakfast In The World</h2>
-							<p>Fabulas relinqueret aut quamquam. Lorem possumus pariatur ut quibusdam transferrem an tempor. E consequat nam senserit, aliquip tamen est commodo illustriora. An cillum sunt ut quamquam. Laboris culpa occaecat, quo fugiat dolore varias consequat</p>
-						</div>
-					</div>
-				</div>
+				{
+					sliders.map(item=>(
+						<SliderItem 
+							key = {item.id}
+							postId = {item.postId}
+						/>
+					))
+				}
 			</div>
 		</div>
 	);
---
+++ outstagram/src/components/widgets/slider/SliderItem.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+function SliderItem({postId}) {
+
+    const [post, setPost] = useState([])
+    const [categoryName, setCategoryName] = useState("")
+    const [slideImageUrl, setSlideImageUrl] = useState("")
+
+    useEffect(()=>{
+        fetch(`http://localhost:3010/post/${postId}`)
+		.then(res=>{
+			return res.json()
+		})
+		.then(data=>{
+			setPost(data)
+			console.log(data.categoryId)
+            setSlideImageUrl(`./images/${data.titleImage}`)
+            callCategoryName(data.categoryId)
+		})
+    },[])
+    
+    const callCategoryName = (id) => {
+        fetch(`http://localhost:3010/categories/${id}`)
+		.then(res=>{
+			return res.json()
+		})
+		.then(data=>{
+			setCategoryName(data.name)
+			console.log(data.name)
+		})
+    }
+
+    return ( 
+        <div>
+			<a className="eskimo-slider-img" href="single-post.html"></a>
+			<ul className="eskimo-slider-image-meta eskimo-image-meta-post">
+                <li><a href="single-post.html"><span className="badge badge-default">{post.created}</span></a></li>
+                <li><a href="category.html"><span className="badge badge-default">{categoryName}</span></a></li>
+            </ul>
+            <div className="clearfix"></div>
+            <img src={slideImageUrl} alt={post.postTitle} />
+            <div className="eskimo-slider-desc">
+                <div className="eskimo-slider-desc-inner">
+                    <h2 className="card-title">{post.postTitle}</h2>
+                    <p>{post.postTitle}</p>
+                </div>
+            </div>
+        </div>
+				
+     );
+}
+
+export default SliderItem;
